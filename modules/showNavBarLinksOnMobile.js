21c3a9771aeda4030756175e2337be39319cf910
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-09-04 17:06:33 +0200

    Toggle main container on click on mobile

--- modules/showNavBarLinksOnMobile.js
+++ modules/showNavBarLinksOnMobile.js
@@ -1,10 +1,12 @@
 // Get DOM elements
 const hamburgerMenu = document.querySelector(".nav-hamburger");
 const navBarMobile = document.querySelector(".nav-links-container-mobile");
+const mainContent = document.querySelector(".planets-main-container");
 
 export default function showNavBarLinksOnMobile() {
   hamburgerMenu.addEventListener("click", () => {
     navBarMobile.classList.toggle("nav-links-container-mobile--show");
     hamburgerMenu.classList.toggle("nav-hamburger--opacity");
+    mainContent.classList.toggle("planets-main-container--hide");
   });
 }
