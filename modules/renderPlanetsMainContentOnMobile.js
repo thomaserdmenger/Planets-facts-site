dd79867e8e20caeb32a5cd3c42839b185e6eac87
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-09-05 06:50:03 +0200

    Add wikipedia on links and border buttom on submenu

--- modules/renderPlanetsMainContentOnMobile.js
+++ modules/renderPlanetsMainContentOnMobile.js
@@ -4,6 +4,8 @@ const navBarLinksOnMobile = document.querySelector(".nav-links-list");
 const mainContent = document.querySelector(".planets-main-container");
 const planetsHeading = document.querySelector(".planets-heading");
 const planetsText = document.querySelector(".planets-text");
+const planetsSource = document.querySelector(".planets-source");
+const planetsMenuOne = document.querySelector(".planets-menu-one");
 
 // Render main content
 export default async function renderPlanetsMainContentOnMobile() {
@@ -23,6 +25,12 @@ export default async function renderPlanetsMainContentOnMobile() {
 
       // Render Planet Description on Screen
       planetsText.textContent = item.overview.content;
+
+      // Add Links to Planet Links
+      planetsSource.setAttribute("href", item.overview.source);
+
+      // Add Border Buttom to Overview in Submenu
+      planetsMenuOne.classList.add("planets-menu-one--active");
     });
   });
 }
