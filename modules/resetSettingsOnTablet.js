8cb5faf305ed0e9623fb939edd23ace8ecf29d8c
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-09-09 16:33:32 +0200

    Update sub menus text on mobile, tablet and dektop

--- modules/resetSettingsOnTablet.js
+++ modules/resetSettingsOnTablet.js
@@ -6,10 +6,10 @@ const planetsMenuThree = document.querySelector(".planets-menu-three-text");
 export default function resetSettingsOnTablet() {
   if (window.innerWidth >= 768) {
     planetsMenuOne.classList.remove("planets-menu-one--active");
-    planetsMenuTwo.textContent = "Internal Structure";
-    planetsMenuThree.textContent = "Surface Geology";
+    // planetsMenuTwo.textContent = "Internal Structure";
+    // planetsMenuThree.textContent = "Surface Geology";
   } else {
-    planetsMenuTwo.textContent = "Structure";
-    planetsMenuThree.textContent = "Surface";
+    // planetsMenuTwo.textContent = "Structure";
+    // planetsMenuThree.textContent = "Surface";
   }
 }
